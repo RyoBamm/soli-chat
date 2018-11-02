e25db6e653d36e1e0d3fcfa70f2558a9e0ac83c3
@RyoBamm <ryooooozrq1205@gmail.com>
2018-11-02 10:49:44 +0900

    Add posting method to controller & Set validations

---
+++ db/migrate/20181102014006_rename_type_column_to_posts.rb
@@ -0,0 +1,5 @@
+class RenameTypeColumnToPosts < ActiveRecord::Migration[5.1]
+  def change
+    rename_column :posts, :type, :post_type
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181029203649) do
+ActiveRecord::Schema.define(version: 20181102014006) do
 
   create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "title", default: "", null: false
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 20181029203649) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "type"
+    t.integer "post_type"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
