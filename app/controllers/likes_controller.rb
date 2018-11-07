b2a8c2fdca99f3c6a3df7b30a3aab5a8222d268d
@RyoBamm <ryooooozrq1205@gmail.com>
2018-11-07 15:57:03 +0900

    Edit some method & views

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,20 +1,22 @@
 class LikesController < ApplicationController
   before_action :set_post
+  before_action :set_likes
 
   def create
     @like = Like.create(user_id: current_user.id, post_id: params[:post_id])
-    @likes = Like.where(post_id: params[:post_id])
     @post.reload
   end
 
   def destroy
     like = Like.find_by(user_id: current_user.id, post_id: params[:post_id])
     like.destroy
-    @likes = Like.where(post_id: params[:post_id])
     @post.reload
   end
 
   private
+  def set_likes
+    @likes = Like.where(post_id: params[:post_id])
+  end
 
   def set_post
     @post = Post.find(params[:post_id])
