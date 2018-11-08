6c356d1d9552d374cc8221acfe6882369d45dfa7
@RyoBamm <ryooooozrq1205@gmail.com>
2018-11-08 12:34:48 +0900

    Add pagination & edit some view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,7 +9,7 @@ class PostsController < ApplicationController
   def show
     @likes = Like.where(post_id: params[:post_id])
     @like = Like.find_by(user_id: current_user.id, post_id: params[:id])
-    @comments = @post.comments.page(params[:page]).per(10).order("created_at DESC")
+    @comments = @post.comments.page(params[:page]).per(5).order("created_at DESC")
     @comment = Comment.new()
   end
 
