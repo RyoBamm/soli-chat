576f04fdd76128706d0e3cb874a6077514dcf946
@RyoBamm <ryooooozrq1205@gmail.com>
2018-11-08 14:44:30 +0900

    Implement pagination by asynchronous communication

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,15 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: [:show, :edit, :update]
+  before_action :set_post_including_user, only: [:create]
 
   def show
   end
 
   def create
     if user_signed_in?
-      @comment = Comment.includes(:user).create(comment_params)
+      comment = Comment.includes(:user).create(comment_params)
+      @comments = @post.comments.page(params[:page]).per(5).order("created_at DESC")
+      @comment = Comment.new()
     else
       redirect_to new_user_session_path
     end
@@ -29,11 +32,15 @@ class CommentsController < ApplicationController
   end
 
   private
+  def comment_params
+    params.require(:comment).permit(:id, :comment).merge(user_id: current_user.id).merge(post_id: params[:post_id])
+  end
+
   def set_comment
     @comment = Comment.includes(:user).find(comment_params[:id])
   end
 
-  def comment_params
-    params.require(:comment).permit(:id, :comment).merge(user_id: current_user.id).merge(post_id: params[:post_id])
+  def set_post_including_user
+    @post = Post.includes(:user).find(params[:post_id])
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,6 +11,10 @@ class PostsController < ApplicationController
     @like = Like.find_by(user_id: current_user.id, post_id: params[:id])
     @comments = @post.comments.page(params[:page]).per(5).order("created_at DESC")
     @comment = Comment.new()
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def new
