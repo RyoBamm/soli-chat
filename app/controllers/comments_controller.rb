c8f4d6e9837bb8b1d3c0dd56bd5e9b86cff0d3a6
@RyoBamm <ryooooozrq1205@gmail.com>
2018-11-09 13:36:44 +0900

    Add editing comments function

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,5 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: [:show, :edit, :update]
-  before_action :set_post_including_user, only: [:create]
 
   def show
   end
@@ -8,10 +7,14 @@ class CommentsController < ApplicationController
   def create
     if user_signed_in?
       comment = Comment.includes(:user).create(comment_params)
-      @comments = @post.comments.page(params[:page]).per(5).order("created_at DESC")
-      @comment = Comment.new()
+      respond_to do |format|
+        format.html{
+          redirect_to post_path(comment.post_id), notice: 'コメントを投稿しました'
+        }
+        format.js
+      end
     else
-      redirect_to new_user_session_path
+      redirect_to post_path(@comment.post_id), alert: 'ログインしてください'
     end
   end
 
@@ -20,14 +23,19 @@ class CommentsController < ApplicationController
 
   def update
     if user_signed_in? && @comment.user_id == current_user.id
-      @comment.update( content: comment_params[:content] )
+      @comment.update( comment: comment_params_for_edit_function[:comment] )
+    else
+      edirect_to post_path(@comment.post_id), alert: 'コメント投稿に失敗しました'
     end
   end
 
   def destroy
-    comment = Comment.find(comment_params[:id])
+    comment = Comment.find(comment_params_for_edit_function[:id])
     if user_signed_in? && comment.user_id == current_user.id
       comment.destroy
+      redirect_to post_path(comment.post_id), notice: 'コメントを削除しました'
+    else
+      redirect_to post_path(comment.post_id), aleat: 'コメント削除に失敗しました'
     end
   end
 
@@ -36,11 +44,11 @@ class CommentsController < ApplicationController
     params.require(:comment).permit(:id, :comment).merge(user_id: current_user.id).merge(post_id: params[:post_id])
   end
 
-  def set_comment
-    @comment = Comment.includes(:user).find(comment_params[:id])
+  def comment_params_for_edit_function
+    params.permit(:id, :comment)
   end
 
-  def set_post_including_user
-    @post = Post.includes(:user).find(params[:post_id])
+  def set_comment
+    @comment = Comment.includes(:user).find(params[:id])
   end
 end
---
+++ app/views/comments/show.json.jbuilder
@@ -0,0 +1,4 @@
+json.id            @comment.id
+json.comment       @comment.comment
+json.post_id       @comment.post_id
+json.user_id       @comment.user_id
