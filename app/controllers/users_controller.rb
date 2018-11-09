47bd008990725955ee5a29f87198928793754f60
@RyoBamm <ryooooozrq1205@gmail.com>
2018-11-09 18:43:53 +0900

    Edit some views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,7 @@ class UsersController < ApplicationController
     redirect_to :root unless user_signed_in? && @user.id == current_user.id
 
     @posts = @user.posts.page(params[:page]).per(4).order("created_at DESC")
-    @posts_count = @user.posts.count
-
+    @user_info = {posts_count: @user.posts.count,all_likes: total_likes_count(@user.id)}
     respond_to do |format|
       format.html
       format.js
@@ -17,5 +16,15 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+# Modelnないため仮置き。ToDo:concern lib化
+  def total_likes_count(user_id)
+    posts = User.find(user_id).posts
+    total_likes_count = 0
+    posts.each do |post|
+      total_likes_count += post.likes_count
+    end
+    return total_likes_count
   end
 end
