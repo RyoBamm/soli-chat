6f871129a6cfd14318fd094057a717ea756b014c
@RyoBamm <ryooooozrq1205@gmail.com>
2018-11-12 00:50:31 +0900

    Modify some views

--- app/assets/javascripts/post_index.js
+++ app/assets/javascripts/post_index.js
@@ -1,9 +1,11 @@
 $(function(){
-  $('.recommended_posts_slider').slick({
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    draggable: true,
-    variableWidth: true,
-    centerMode: true,
+  $(document).on('turbolinks:load', function() {
+    $('.recommended_posts_slider').slick({
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      draggable: true,
+      variableWidth: true,
+      centerMode: true,
+    });
   });
 });
