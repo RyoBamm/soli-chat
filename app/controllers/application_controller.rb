02516f1ef84818484fcb7361f12743d16489a0d1
@RyoBamm <ryooooozrq1205@gmail.com>
2018-11-15 21:23:20 +0900

    Modify stoppage view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   protected
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :age, :avatar, :profile, :prefecture_id])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :age, :avatar])
       devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :age, :avatar, :profile, :prefecture_id])
     end
 end
